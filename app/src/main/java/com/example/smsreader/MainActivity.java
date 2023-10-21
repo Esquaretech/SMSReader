9857af231a6e304748e8de82aee11f4b4c2b34e9
Esqauretech <esquaretech2023@gmail.com>
2023-10-21 23:30:42 +0530

    Now user can view the date,time,amount,receiver for their today's transaction and description as user can give through textbox then that will store at the internal storage of the users mobile  the file name will be saved as today's date.

--- app/src/main/java/com/example/smsreader/MainActivity.java
+++ app/src/main/java/com/example/smsreader/MainActivity.java
@@ -2,78 +2,134 @@ package com.example.smsreader;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Telephony;
 import java.util.Calendar;
+import android.widget.AutoCompleteTextView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
-public class MainActivity extends Activity {
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends Activity {
+    private TextView smsTextView;
+    private Button savebutton;
+    public String jsonData;
+    private String hdfcSms;
+    public String DateOfToday;
+    public String formattedDate;
+    public String formattedTime;
+    public String amount;
+    public String receiver;
+    public String description;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView smsTextView = findViewById(R.id.smsTextView);
+        smsTextView = findViewById(R.id.smsTextView);
+        savebutton = findViewById(R.id.button);
 
-        // Get the current date
         Calendar calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
         int month = calendar.get(Calendar.MONTH) + 1; // Note: Months are zero-based, so add 1.
         int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-        // Define the desired date in the format "yyyy-MM-dd"
-        String desiredDate = day + "-" + month + "-" + year;
+        DateOfToday = day + "-" + month + "-" + year;
+        //DateOfToday = "13-10-2023"; //testing date
 
-
-        // Read and filter HDFC Bank SMS messages for the desired date
-        String hdfcSms = readHDFCSms(desiredDate);
-
-        // Update the TextView with the HDFC Bank SMS messages
+        hdfcSms = readHDFCSms(DateOfToday);
         smsTextView.setText(hdfcSms);
 
-    }
+        savebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hdfcSms = readHDFCSms(DateOfToday);
 
-    private String readHDFCSms(String desiredDate) {
+                String FILE_NAME = DateOfToday ;
+                try {
+                    JSONObject jsonObject = new JSONObject();
+                    jsonObject.put("Date", formattedDate);
+                    jsonObject.put("Time", formattedTime);
+                    jsonObject.put("Amount", amount);
+                    jsonObject.put("Receiver", receiver);
+                    jsonObject.put("Description", description);
+
+                   jsonData = jsonObject.toString();
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                try {
+
+                    String filePath = "/storage/emulated/0/Download/"+FILE_NAME+".json";
+
+                    FileOutputStream fos = new FileOutputStream(filePath);
+                    fos.write(jsonData.getBytes());
+                    fos.close();
+
+
+                    Toast.makeText(getApplicationContext(), "Data saved to Downloads directory", Toast.LENGTH_LONG).show();
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+    }
+    private String readHDFCSms(String DateOfToday) {
         Uri uri = Uri.parse("content://sms/inbox");
         Cursor cursor = getContentResolver().query(uri, null, "body LIKE ?", new String[]{"%HDFC Bank%"}, null);
+
         if (cursor != null && cursor.moveToFirst()) {
             StringBuilder hdfcSms = new StringBuilder();
             do {
                 @SuppressLint("Range") String body = cursor.getString(cursor.getColumnIndex(Telephony.Sms.BODY));
-
                 @SuppressLint("Range") String date = cursor.getString(cursor.getColumnIndex(Telephony.Sms.DATE));
                 String particularDate = date;
-                // Parse the date from milliseconds to "yyyy-MM-dd" format
                 SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-                String formattedDate = dateFormat.format(new Date(Long.parseLong(particularDate)));
+                formattedDate = dateFormat.format(new Date(Long.parseLong(particularDate)));
 
-                // Check if the parsed date matches the desired date
-                if (formattedDate.equals(desiredDate)) {
-                    Pattern pattern = Pattern.compile("Money Transfer:Rs (\\d+\\.\\d+) from .* on (\\d{2}-\\d{2}-\\d{2}) to (.+)");
+                String timeFormat = "h:mm a"; // Define the desired time format
+                SimpleDateFormat timeFormatter = new SimpleDateFormat(timeFormat);
+
+                formattedTime = timeFormatter.format(new Date(Long.parseLong(particularDate)));
+
+                if (formattedDate.equals(DateOfToday)) {
+                    Pattern pattern = Pattern.compile("Money Transfer:Rs (\\d+\\.\\d+) from .* on (\\d{2}-\\d{2}-\\d{2}) to (.+) UPI: (\\d+) Not you?");
+
                     Matcher matcher = pattern.matcher(body);
                     if (matcher.find()) {
-                        // Extract the matched information
-                        String amount = matcher.group(1);
-//                        String date = matcher.group(2);
-                        String receiver = matcher.group(3);
+                        amount = matcher.group(1);
+                        receiver = matcher.group(3);
 
-                        hdfcSms.append("Date: ").append(formattedDate).append("\nAmount: ₹").append(amount).append("\nReceiver: ").append(receiver).append("\n\n");
+                        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+                        description = autoCompleteTextView.getText().toString();
+
+                        hdfcSms.append("Date: ").append(formattedDate).append("\nTime: ").append(formattedTime).append("\nAmount: ₹").append(amount).append("\nReceiver: ").append(receiver).append("\nDescription: ").append(description).append("\n\n");
 
                     } else {
                         System.out.println("No match found in the SMS.");
                     }
-
-
-
                 }
             } while (cursor.moveToNext());
 
@@ -82,10 +138,9 @@ public class MainActivity extends Activity {
             if (hdfcSms.length() > 0) {
                 return hdfcSms.toString();
             } else {
-                return "No HDFC Bank SMS messages found for " + desiredDate;
+                return "No HDFC Bank SMS messages found for " + DateOfToday;
             }
         }
-
-        return "No HDFC Bank SMS messages found for " + desiredDate;
+        return "No HDFC Bank SMS messages found for " + DateOfToday;
     }
 }
